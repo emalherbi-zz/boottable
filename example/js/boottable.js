31b43b8f6e551c0ca826224f32525e2edbac8d33
Eduardo Malherbi Martins <emalherbi@gmail.com>
2014-04-28 09:10:27 -0300

    .

--- example/js/boottable.js
+++ example/js/boottable.js
@@ -1,11 +1,6 @@
 /*!
- * Bootstrap v3.0.1 by @fat and @mdo
- * Copyright 2013 Twitter, Inc.
- * Licensed under http://www.apache.org/licenses/LICENSE-2.0
- *
- * Designed and built with all the love in the world by @mdo and @fat.
+ * bootTable v0.0.1 by emalherbi.com
  */
-
 (function($){
 	if(!$.Table){
 		$.Table = new Object();
